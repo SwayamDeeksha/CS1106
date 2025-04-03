58140c1c9d728446c6dfe1f836be109dfd1d4880
Swayam Deeksha <swadeeku.007@gmail.com>
2025-04-03 10:42:12 +0530

    Create Backup.sh

---
+++ Lab 8 - Practice Questions/Backup.sh
@@ -0,0 +1,9 @@
+# Create a new folder called backup
+echo "Backup folder creating..."
+mkdir Backup
+echo "Backup folder created."
+
+# Copying all .txt files to backup folder
+echo "Copying all text files to Backup folder..."
+cp *.txt Backup
+echo "Copied all text files to Backup folder."
